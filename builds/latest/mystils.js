aa283e66ec5e20cf67c591497dbeed7b24030c6e
MysticalEnnio <mystical.ennio@gmail.com>
2022-01-18 14:02:11 +0100

    Update mystils.js

--- builds/latest/mystils.js
+++ builds/latest/mystils.js
@@ -190,8 +190,6 @@ class mystils {
                 }
 
                 div {
-                    width: 100%;
-                    height: 100%;
                     margin: 0;
                     padding: 0;
                 }
